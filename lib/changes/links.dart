96c1ee32bc96def1679f2091e373b2110f955566
Elvis888361 <elvisndegwa90@gmail.com>
2023-06-21 18:01:25 +0300

    first commit

---
+++ lib/changes/links.dart
@@ -0,0 +1,38 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1tYW0OGebARYmn8NX-j40gv-ZkFG1aAjC/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/Elvis888361/Elvis888361';
+// Github Link
+const String facebook = 'https://kangetheelvis.netlify.app/';
+// Github Link
+const String twitter = 'https://twitter.com/elvis_ndegwa01';
+// Github Link
+const String youtube =
+    'https://kangetheelvis.netlify.app/';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/elvis-ndegwa-b0b785197';
+// Github Link
+const String buymeacoffee = 'https://www.buymeacoffee.com/sudeshnb';
+// Github Link
+const String instagram = 'https://instagram.com/elvisndesh';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/sudeshnb",
+//   // "https://instagram.com/sudesh_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/sudesh78",
+//   "https://linkedin.com/in/sudeshnb",
+//   "https://www.buymeacoffee.com/sudeshnb", "https://github.com/sudeshnb",
+// ];
+//
+const String profileImage =
+                "https://kangetheelvis.netlify.app/";
+//
+// Github Link
+const String fiverr = 'https://kangetheelvis.netlify.app/';
+// Github Link
+const String upwork = 'https://kangetheelvis.netlify.app/';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=254745638455';
